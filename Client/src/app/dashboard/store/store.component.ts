47c028e9bb92e836d7787e4f909b614b31382a51
OmarElkilany <omar.alkilani23@gmail.com>
2018-02-26 23:34:46 +0200

    Added Checks and error messages. Search Remains.

--- Client/src/app/dashboard/store/store.component.ts
+++ Client/src/app/dashboard/store/store.component.ts
@@ -7,13 +7,14 @@ import { StoreService } from './store.service';
   templateUrl: './store.component.html',
   styleUrls: ['./store.component.scss']
 })
-export class StoreComponent implements OnInit {
 
+export class StoreComponent implements OnInit {
   source: any[];
   createMode = false;
   product: any = {};
-
   toUpdateProduct: any = {};
+
+  error: String;
 
   constructor(private storeService: StoreService) {
   }
@@ -41,29 +42,55 @@ export class StoreComponent implements OnInit {
   leaveCreateMode(): void {
     this.createMode = false;
     this.product = {};
+    this.error = null;
   }
 
   createProduct(): void {
+
+    if (!this.product.name || !this.product.price || !this.product.sellerName) {
+      this.error = 'Please supply all the required fields.';
+      return;
+    }
+
+    if (isNaN(Number(this.product.price))) {
+      this.error = 'The Price should be a valid number.';
+      return;
+    }
+
     this.storeService.createProduct(this.product)
       .subscribe(prod => this.source.push(prod.data));
-      this.product={};
+    this.product = {};
     this.leaveCreateMode();
+    this.error = null;
   }
 
   setToUpdateProduct(product: any): void {
-    this.toUpdateProduct = product;
+    this.toUpdateProduct = Object.assign({}, product);
   }
 
   updateProduct(product: any): void {
+    if(!product.name||!product.price||!product.sellerName){
+      this.error = "Please don't leave empty fields after your edit";
+      return;
+    }
+
+    if (isNaN(Number(product.price))) {
+      this.error = 'The Price should be a valid number after your edit.';
+      return;
+    }
+
     this.storeService.updateProduct(product._id, product)
       .subscribe(prod => {
-      this.toUpdateProduct = {};
-      product.updatedAt = prod.data.updatedAt;
+        this.toUpdateProduct = {};
+        product.updatedAt = prod.data.updatedAt;
       });
   }
 
-  cancelUpdate(): void {
-    this.toUpdateProduct = {};
+  cancelUpdate(product: any): void {
+    product.name = this.toUpdateProduct.name;
+    product.sellerName = this.toUpdateProduct.sellerName;
+    product.price =this.toUpdateProduct.price;
+    this.toUpdateProduct = Object.assign({}, {});
+    this.error = null;
   }
-
 }
